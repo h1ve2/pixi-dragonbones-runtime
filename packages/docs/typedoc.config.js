a87723770b0b6c9b4d7fb508a931c955b0e29dfd
h1ve2 <xy@h1v.cn>
2025-04-07 18:37:19 +0800

    exclude externals

--- packages/docs/typedoc.config.js
+++ packages/docs/typedoc.config.js
@@ -15,6 +15,7 @@ const config = {
     sourceLinkTemplate: 'https://github.com/h1ve2/pixi-dragonbones-runtime/blob/{gitRevision}/packages/pixi-dragonbones-runtime/src/animation/Animation.ts#L{line}',
     excludePrivate: true,
     excludeProtected: true,
+    excludeExternals: true,
     "expandObjects": false,
     "expandParameters": true,
     indexFormat:'table',
